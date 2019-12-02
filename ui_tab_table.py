5881c36bdd3539a758166b7094987a14d35827b2
Ashley Sands <ashsands1@amstrike.com>
2019-12-02 23:20:23 +0000

    Added tab count and auto name tabs

--- ui_tab_table.py
+++ ui_tab_table.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from ui_helpers import UiHelpers
 
 class ui_tabTable:
 
@@ -10,8 +11,11 @@ class ui_tabTable:
         self.translate = QtCore.QCoreApplication.translate
         self.tab_widget = tab_widget
         self.tab_widget.tabCloseRequested.connect( self.close_tab );
+        self.tab_count = 0;
 
-    def add_tab(self, object_name, name):
+        self.help = UiHelpers()
+
+    def add_tab(self, name):
         """ Adds new tab to table widget
 
         :param object_name:     name of the object (should be unique to app)
@@ -20,13 +24,20 @@ class ui_tabTable:
         :return:                the new tab
         """
 
-
         tab = QtWidgets.QWidget()
-        tab.setObjectName( object_name )
+        tab.setObjectName( "tab_"+str(self.tab_count) )
+
         self.tab_widget.addTab(tab, "")
         self.tab_widget.setTabText(self.tab_widget.indexOf(tab), self.translate("MainWindow", name))
+
+        # create table view
+        table = self.help.create_table_widget(tab, "table_" + str(self.tab_count), (0, 15, 600, 371))
+        self.help.set_table_columns(table, ["a", "b", "c", "d", "e"])
+
         # set active!
         self.tab_widget.setCurrentIndex(self.tab_widget.indexOf(tab))
+
+        self.tab_count += 1
 
         return tab
 
