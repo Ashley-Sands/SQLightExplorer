cd78d37b7ca4722d7daf300fcd7891edfb968ec1
Ashley Sands <ashsands1@amstrike.com>
2019-12-02 22:27:56 +0000

    Added Close Signal and set tab active when added

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import amsql_viewer_ui
 import ui_helpers
-
-
-
+from ui_tab_table import ui_tabTable
 
 if __name__ == "__main__":
 
     import sys
     from PyQt5 import QtCore, QtGui, QtWidgets
 
+    # start the QApp
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-
     main_app = amsql_viewer_ui.Ui_MainWindow()
     main_app.setupUi(MainWindow)
+
+    # Add the welcome tab
     main_app.welcome_tab()
 
+    # Setup the classes to manage the UI elements :)
+    ui_tab_table = ui_tabTable( main_app.tab_view )
+
+
+
+    # Finaly show the window :D
     MainWindow.show()
 
     sys.exit(app.exec_())
--- ui_tab_table.py
+++ ui_tab_table.py
@@ -2,15 +2,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class ui_tabTable:
 
-    def __init__(self, tab_widget):
+    def __init__(self, tab_widget ):
         """ Stores tab widget and manages tabs
 
         :param tab_widget:  the widget that contains tabs.
         """
         self.translate = QtCore.QCoreApplication.translate
-
         self.tab_widget = tab_widget
-        self.tab_widget.tabCloseRequest.connect(self.close_tab)
+        self.tab_widget.tabCloseRequested.connect( self.close_tab );
 
     def add_tab(self, object_name, name):
         """ Adds new tab to table widget
@@ -26,8 +25,10 @@ class ui_tabTable:
         tab.setObjectName( object_name )
         self.tab_widget.addTab(tab, "")
         self.tab_widget.setTabText(self.tab_widget.indexOf(tab), self.translate("MainWindow", name))
+        # set active!
+        self.tab_widget.setCurrentIndex(self.tab_widget.indexOf(tab))
 
         return tab
 
     def close_tab(self, tab_index):
-        self.tab_widget.tabWidget.removeTab(tab_index)
+        self.tab_widget.removeTab(tab_index)
