5e90b5e11602af654d891136f54221e549283f4c
Ashley Sands <ashsands1@amstrike.com>
2019-12-03 12:14:23 +0000

    Fixed class name not being updated

--- amsql_warning_window.py
+++ amsql_warning_window.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
+    ui = UiWarningDialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
