5a0ac15c4b35ff32514b37a436359a5962ab8b0d
Ashley Sands <ashsands1@amstrike.com>
2019-12-03 12:32:52 +0000

    Changed DialogueWindow into a base class and added Warning and text enter child classes

--- dialogue_window.py
+++ dialogue_window.py
@@ -4,25 +4,73 @@ import amsql_text_window
 
 class DialogueWindow:
 
+    DIALOG_STATUS_NONE = 0
+    DIALOG_STATUS_ACCEPTED = 1
+    DIALOG_STATUS_REJECTED = 2
+
     def __init__(self):
+        self.dialog = None  # Dialog window
+        self.app = None     # App class (access to UI elements)
+        self.dialogStatus = self.DIALOG_STATUS_NONE
+
+    def new_window(self):
+
+        self.reset_status()
+        self.set_signals()
+
+    def set_signals(self):
+        self.app.button_yes.accepted.connect( self.dialog_accepted() )
+        self.app.button_yes.rejected.connect( self.dialog_rejected() )
+
+    def reset_status(self):
         self.dialog = None
+        self.app = None
+        self.dialogStatus = self.DIALOG_STATUS_NONE
 
-    def new_warning_window(self):
+    def dialog_accepted(self):
 
-        #if self.dialog != None:  # bug: not clearing
-        #    return
+        self.dialog = None
+        pass
 
+    def dialogu_rejected(self):
+
+        self.dialog = None
+        pass
+
+
+class DialogueWindow_Warning(DialogueWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.text = ""
+
+    def new_window(self):
+
+        if self.dialog != None:
+            return
+
+        super().new_window()
+
+        # create new dialogue window
         self.dialog = QtWidgets.QDialog()
-        warning_app = amsql_warning_window.UiWarningDialog()
-        warning_app.setupUi(self.dialog)
+        self.app = amsql_warning_window.UiWarningDialog()
+        self.app.setupUi(self.dialog)
         self.dialog.show()
 
-    def new_text_window(self):
+class DialogueWindow_TextEnter(DialogueWindow):
 
-        #if self.dialog != None:  # bug: not clearing
-        #    return
+    def __init__(self):
+        super().__init__()
 
+    def new_window(self):
+
+        if self.dialog != None:
+            return
+
+        super().new_window()
+
+        # create out text input window
         self.dialog = QtWidgets.QDialog()
-        ui = amsql_text_window.UiTextDialog()
-        ui.setupUi(self.dialog)
+        self.app = amsql_text_window.UiTextDialog()
+        self.app.setupUi(self.dialog)
         self.dialog.show()
