d02836fee84d8152c74a5fd4cb956459b0960494
Ashley Sands <ashsands1@amstrike.com>
2019-12-03 00:22:52 +0000

    temp bug fix

--- dialogue_window.py
+++ dialogue_window.py
@@ -9,8 +9,8 @@ class DialogueWindow:
 
     def new_warning_window(self):
 
-        if self.dialog != None:
-            return
+        #if self.dialog != None:  # bug: not clearing
+        #    return
 
         self.dialog = QtWidgets.QDialog()
         warning_app = amsql_warning_window.UiWarningDialog()
@@ -19,8 +19,8 @@ class DialogueWindow:
 
     def new_text_window(self):
 
-        if self.dialog != None:
-            return
+        #if self.dialog != None:  # bug: not clearing
+        #    return
 
         self.dialog = QtWidgets.QDialog()
         ui = amsql_text_window.UiTextDialog()
