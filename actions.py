a013cf4c42f17ec312610cce4966cf0e7aef243c
Ashley Sands <ashsands1@amstrike.com>
2019-12-03 23:22:43 +0000

    made base class better and added new database

--- actions.py
+++ actions.py
@@ -2,40 +2,64 @@ from web_querys import WebQuerys
 
 class Action:
 
-    def dialog_action(self, dialog, accepted):
-        pass
-
-    def action(self, response_data):
-        pass
-
-
-class Action_OpenDatabase(Action):
-    def __init__(self, dialog_message, tree_view):
+    def __init__(self, dialog_message, message404="Error: Not Found"):
         self.dialog_message = dialog_message
-        self.tree_view = tree_view
+        self.message404 = message404
+        self.web_query = WebQuerys()
+
+    def request(self, dialog):
+        """
+
+        :return: request responce
+        """
+        pass
 
     def dialog_action(self, dialog, accepted):
         if accepted == 0:
             return
 
-        web_query = WebQuerys()
-        response = web_query.open_database(dialog.text)
+        response = self.request(dialog)
 
         if str(response[0]) == "404":
-            self.dialog_message.set_message("Error: Database Not Found")
+            self.dialog_message.set_message(self.message404)
             self.dialog_message.new_window()
         elif str(response[0]) == "408":
             self.dialog_message.set_message("Error: Connection Timed Out :(")
             self.dialog_message.new_window()
         else:
-            self.action( response[1] )
+            self.action(dialog, response[1])
+
+    def action(self, dialog, response_data):
+        pass
+
+
+class Action_NewDatabase(Action):
+
+    def __init__(self, dialog_message, tree_view, message404="Not Found"):
+        super().__init__(dialog_message, message404)
+        self.tree_view = tree_view
+
+    def request(self, dialog):
+        return self.web_query.new_database(dialog.text)
 
     def action(self, dialog, response):
 
         # Add the data to the tree
         self.tree_view.add_tree_item(None, dialog.text)  # add database
+
+
+class Action_OpenDatabase(Action_NewDatabase):
+
+    def request(self, dialog):
+        return self.web_query.open_database(dialog.text)
+
+    def action(self, dialog, response):
+
+        # Add the data to the tree
+        super().action(dialog, response)
         for r in response:
             self.tree_view.add_tree_item(dialog.text, r)
+
 
 
 
