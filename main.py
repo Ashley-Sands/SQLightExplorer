dbc99ed4a9fccd5526160b24a4f5649f845c4df9
Ashley Sands <ashsands1@amstrike.com>
2019-12-03 22:45:38 +0000

    move dialogues into the setup sequance

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from ui_objects.ui_tree_view import UiTreeView
 from dialogue_window import DialogueWindow_Warning, DialogueWindow_TextEnter, DialogueWindow_Config, DialogueWindow_Message
 from web_querys import WebQuerys
 
-
-dialog_message = DialogueWindow_Message("message")
-
 def dialogue_callback( dialog_name, accepted ):
-
+    pass
+'''
     if accepted == 0:
         return
 
@@ -23,24 +21,11 @@ def dialogue_callback( dialog_name, accepted ):
             dialog_message.set_message("Error: Connection Timed Out :(")
             dialog_message.new_window()
         else:
+            # Add the data to the tree
             pass
 
     print("data:", response[0], response[1])
-
-
-dialogs = {}
-
-# create our dialogue instances
-dialogs["drop_table"] = DialogueWindow_Warning("drop_table", dialogue_callback)
-dialogs["new_database"] = DialogueWindow_TextEnter("new_database", dialogue_callback)
-dialogs["open_database"] = DialogueWindow_TextEnter("open_database", dialogue_callback)
-dialogs["config"] = DialogueWindow_Config("config")
-
-# set dict of dialogues in each dialogue instance to prevent multiple windows from being opened
-dialogs["drop_table"].set_dialog_windows(dialogs)
-dialogs["new_database"].set_dialog_windows(dialogs)
-dialogs["open_database"].set_dialog_windows(dialogs)
-dialogs["config"].set_dialog_windows(dialogs)
+'''
 
 if __name__ == "__main__":
     import amsql_config # setup config
@@ -60,6 +45,25 @@ if __name__ == "__main__":
     ui_tab_table = ui_tabTable( main_app.tab_view )
     ui_tree_view = UiTreeView(main_app.treeWidget, ui_tab_table)
 
+    # Setup message dialog window
+    dialog_message = DialogueWindow_Message("message")
+
+    # Setup actions
+
+    # Setup dialogue instances
+    dialogs = {}
+
+    dialogs["drop_table"] = DialogueWindow_Warning("drop_table", dialogue_callback)  #TODO: Remove name from class / __init__
+    dialogs["new_database"] = DialogueWindow_TextEnter("new_database", dialogue_callback)
+    dialogs["open_database"] = DialogueWindow_TextEnter("open_database", dialogue_callback)
+    dialogs["config"] = DialogueWindow_Config("config")
+
+    # set dict of dialogues in each dialogue instance to prevent multiple windows from being opened
+    dialogs["drop_table"].set_dialog_windows(dialogs)
+    dialogs["new_database"].set_dialog_windows(dialogs)
+    dialogs["open_database"].set_dialog_windows(dialogs)
+    dialogs["config"].set_dialog_windows(dialogs)
+
     # bind buttons
     main_app.button_drop_table.clicked.connect(dialogs["drop_table"].new_window)
     main_app.button_new_database.clicked.connect(dialogs["new_database"].new_window)
@@ -70,7 +74,7 @@ if __name__ == "__main__":
     main_app.actionSettings.triggered.connect(dialogs["config"].new_window)
     main_app.actionQuit.triggered.connect(quit)
 
-    # Finaly show the window :D
+    # Finally show the window :D
     MainWindow.show()
 
     sys.exit(app.exec_())
