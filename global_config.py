8d6d933175f3d217c181ebd6fab1fccd7e4030c4
Ashley Sands <ashsands1@amstrike.com>
2019-12-03 15:32:03 +0000

    Changed dict to only use lowercase for keys

--- global_config.py
+++ global_config.py
@@ -5,11 +5,10 @@ class GlobalConfig:
     @staticmethod
     def get(config_name):
         """Gets the config data for name"""
-        if config_name in GlobalConfig.__CONFIG__:
-            return GlobalConfig.__CONFIG__[config_name]
+        if config_name.lower() in GlobalConfig.__CONFIG__:
+            return GlobalConfig.__CONFIG__[config_name.lower()]
 
     @staticmethod
     def set(config_name, data):
         """Sets or adds (if not exist) to the global config"""
-        GlobalConfig.__CONFIG__[config_name] = data
-
+        GlobalConfig.__CONFIG__[config_name.lower()] = data
