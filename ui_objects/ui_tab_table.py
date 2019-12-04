776a0e62416321866910cedf212d6dc0732ea961
Ashley Sands <ashsands1@amstrike.com>
2019-12-04 21:38:05 +0000

    added set columns

--- ui_objects/ui_tab_table.py
+++ ui_objects/ui_tab_table.py
@@ -3,7 +3,7 @@ from ui_objects.ui_helpers import UiHelpers
 
 class ui_tabTable:
 
-    def __init__(self, tab_widget ):
+    def __init__(self, tab_widget):
         """ Stores tab widget and manages tabs
 
         :param tab_widget:  the widget that contains tabs.
@@ -11,7 +11,8 @@ class ui_tabTable:
         self.translate = QtCore.QCoreApplication.translate
         self.tab_widget = tab_widget
         self.tab_widget.tabCloseRequested.connect( self.close_tab );
-        self.tabs = {}      # key tab names (tab, table)
+
+        self.tabs = {}      # key tab names tuple(tab, table)
         self.tab_count = 0;
 
         self.help = UiHelpers()
@@ -34,7 +35,6 @@ class ui_tabTable:
 
         # create table view
         table = self.help.create_table_widget(tab, "table_" + str(self.tab_count), (0, 15, 600, 371))
-        self.help.set_table_columns(table, ["a", "b", "c", "d", "e"])
 
         # set active!
         self.tab_widget.setCurrentIndex(self.tab_widget.indexOf(tab))
@@ -43,6 +43,19 @@ class ui_tabTable:
         self.tab_count += 1
 
         return tab
+
+    def set_table_columns(self, tab_name, column_names):
+        """ Sets column names for tab with tab_name
+
+        :param tab_name:        name of tab
+        :param column_names:    List of column names
+        :return:                None
+        """
+
+        if tab_name not in self.tabs:
+            return None
+
+        self.help.set_table_columns(self.tabs[tab_name][1], column_names)
 
     def close_tab(self, tab_index):
 
