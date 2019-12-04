85f5030848580b51d08120ccb870f3aec0664514
Ashley Sands <ashsands1@amstrike.com>
2019-12-04 22:14:39 +0000

    added method to verafi data from server

--- actions.py
+++ actions.py
@@ -32,11 +32,18 @@ class Action:
         elif str(response[0]) == "408":
             self.dialog_message.set_message("Error: Connection Timed Out :(")
             self.dialog_message.new_window()
+        elif not self.valid_response_data(response[1]):
+            self.dialog_message.set_message("Error: Invalid response :(")
+            self.dialog_message.new_window()
         else:
             self.action(data_object, response[1])
 
     def action(self, data_object, response_data):
         pass
+
+    def valid_response_data(self, response):
+        """ Checks if response is vails (needs overriding)"""
+        return False;
 
 class Action_NewDatabase(Action):
 
@@ -52,6 +59,8 @@ class Action_NewDatabase(Action):
         # Add the data to the tree
         self.tree_view.add_tree_item(None, dialog.text)  # add database
 
+    def valid_response_data(self, response):
+        return True
 
 class Action_OpenDatabase(Action_NewDatabase):
 
@@ -65,9 +74,38 @@ class Action_OpenDatabase(Action_NewDatabase):
         for r in response:
             self.tree_view.add_tree_item(dialog.text, r)
 
-class Action_GetTableColumns(Action):
+    def valid_response_data(self, response):
+        return type(response) is list
+
+class Action_TableColumns(Action):
+
+    def __init(self, dialog_message, tab_table ):
+        super.__init__(dialog_message)
+        self.tab_table = tab_table
 
     def request(self, data_object):
-        return self.web_query.get_column_names(  )
+        """
 
+        :param data_object:     dict with keys 'database_name' and 'table_name'
+        :return:                request results
+        """
+        return self.web_query.get_column_names(data_object["database_name"], data_object["table_name"])
 
+    def action(self, data_object, response):
+        """
+
+        :param data_object:     dict with keys 'database_name' and 'table_name'
+        :param response:        data from request
+        :return:                None
+        """
+
+        # the data in the response contains all column data (ie, name, type, ...)
+        # we only need the first and seconds elements one for the column names
+        column_names = []
+        for r in response:
+            column_names.append(r[0] + "(" + r[1] + ")" )
+
+        self.tab_table.set_table_columns("table:"+data_object["table_name"], column_names)
+
+    def valid_response_data(self, response):
+        return type(response) is list and type(response[0]) is list
