0e0261f9aea30b9e8a11ca808a3b05ef0652b35b
Ashley Sands <ashsands1@amstrike.com>
2019-12-10 21:59:32 +0000

    removed old vars

--- ui_objects/ui_tab_table.py
+++ ui_objects/ui_tab_table.py
@@ -18,15 +18,10 @@ class ui_tabTable:
 
         self.selected_cel = None
         self.selected_cel_value = ""    # used to restore value if invalid value is inputed
-        self.item_changed_action = []
 
         self.tabs = {}      # key tab names, tuple(tab, table)
-        self.tab_data = {}  # key tab names, dict{type, db_name, table_name, (table_columns_names)}
-
-        self.table_column_parmas = {}   # key tab name. (editable, input value type, default_value )
-        self.tab_count = 0;
-
-        self.setting_table_data = False
+        self.tab_data = {}  # key tab names, dict{type, db_name, table_name}
+        self.tab_count = 0
 
         self.status_bar = status_bar
         self.help = UiHelpers()
@@ -34,9 +29,6 @@ class ui_tabTable:
     @staticmethod
     def get_tab_name(tab_type, db_name, table_name):
         return tab_type + ":" + db_name + "." + table_name
-
-    def add_item_changed_action(self, action):
-        self.item_changed_action.append(action)
 
     def get_tab_data(self, tab_name):
 
@@ -74,7 +66,7 @@ class ui_tabTable:
         self.tab_widget.setCurrentIndex(self.tab_widget.indexOf(tab))
 
         self.tabs[name.lower()] = tab, table
-        self.tab_data[name.lower()] = {"type": tab_type, "db_name": database_name, "table_name": table_name, "table_column_names": ()}
+        self.tab_data[name.lower()] = {"type": tab_type, "db_name": database_name, "table_name": table_name}
         self.tab_count += 1
 
         return tab, table
@@ -109,53 +101,6 @@ class ui_tabTable:
 
         return None, None
 
-    def cell_selected(self, item, prv_item):
-        self.selected_cel = item
-        self.selected_cel_value = item.text()
-
-    def cell_changed(self, item):
-        """ signal/callback when cell changes in table """
-
-        current_tab_name = self.get_current_tab_name()
-
-        if self.setting_table_data is True or self.tab_data["type"] != ui_tabTable.TAB_TYPE_TABLE:
-            return
-
-        tab, table = self.get_tab_table_from_table_item(item)
-
-        if tab is None:
-            self.status_bar.showMessage("Error: tab not found. can not change cell", 20000)
-            return
-
-        tab_name = self.tab_widget.tabText( self.tab_widget.indexOf(tab) )
-        db_name, table_name = self.get_database_and_table_name()
-        valid_data = self.verify_value(item.text(), self.table_column_parmas[tab_name.lower()][item.column()][1])
-        set_column_name = table.horizontalHeaderItem( item.column() ).text()
-
-        if not valid_data:
-            item.setText(self.selected_cel_value)
-            self.status_bar.showMessage("Error: Invalid data type", 20000)
-            return
-        else:
-            self.selected_cel_value = item.text()   # updated the selected value if valid
-
-        # get the rowid value
-        rowid_value = table.item(item.row(), 0).text()                  # TODO: this will only use the first column for the WHERE when updateing cells
-        where_column_name = table.horizontalHeaderItem(0).text()
-
-        print(item.text(), item.row(), item.column(), where_column_name, "=", rowid_value)
-
-        action_data = {}
-        action_data["database_name"] = db_name
-        action_data["table_name"] = table_name
-        action_data["set_columns"] = [set_column_name]
-        action_data["set_data"] = [item.text()]
-        action_data["where_columns"] = [where_column_name]
-        action_data["where_data"] = [rowid_value]
-
-        for act in self.item_changed_action:
-            act.run_action( action_data , 1 )
-
     def get_database_and_table_name(self):
         """Gets the current database and table for selected table"""
         current_tab_name = self.get_current_tab_name()
@@ -163,7 +108,6 @@ class ui_tabTable:
             return self.tab_data[current_tab_name]["db_name"], self.tab_data[current_tab_name]["table_name"]
         else:
             return None, None
-
 
     def verify_value(self, value, value_type):
         """Verifies that the values matches the column value type"""
@@ -185,12 +129,8 @@ class ui_tabTable:
 
     def close_tab(self, tab_index):
         # delete tab data
-        if  self.tab_widget.tabText(tab_index) in self.tabs:
+        if self.tab_widget.tabText(tab_index) in self.tabs:
             del self.tabs[self.tab_widget.tabText(tab_index)]
-
-        # delete table column data.
-        if  self.tab_widget.tabText(tab_index) in self.table_column_parmas:
-            del self.table_column_parmas[ self.tab_widget.tabText(tab_index) ]
 
         # bye bye, tab :)
         self.tab_widget.removeTab(tab_index)
